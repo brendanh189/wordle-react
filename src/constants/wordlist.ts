2b454038471cd6f04c9aaa0d2222abf89130b13f
brendanh189 <brendanh189@berkeley.edu>
2022-12-31 18:58:36 -0800

    shuffled words

--- src/constants/wordlist.ts
+++ src/constants/wordlist.ts
@@ -5757,3 +5757,22 @@ export const WORDS = [
   'biffy',
   'pupal',
 ]
+
+function shuffle(array: any[]) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+export const SHUFFLED = shuffle(WORDS)
